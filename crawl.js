ee3adc133df734abd699e107837c01f5056672e5
Ashish Yadav <savitayadavmansarover@gmail.com>
2026-01-11 03:28:05 +0530

    Updated JSDOM version to support jest

--- crawl.js
+++ crawl.js
@@ -1,4 +1,4 @@
-
+const { JSDOM } = require("jsdom");
 /* Normalize the URL to remove the trailing slash and convert the URL to lowercase
 Examples:
 https://boot.dev -> https://boot.dev
@@ -15,6 +15,21 @@ function normalizeUrl(urlString) {
     return hostPath;
 }
 
+// JSDOM -> JSDOM give us a DOM object from a HTML string
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll("a"); // assuming all links are in <a> tags
+    for(const linkElement of linkElements){
+        const href = linkElement.href;
+        // const normalizedHref = normalizeUrl(href);
+        // urls.push(normalizedHref);
+        urls.push(href);
+    }
+    return urls;
+}
+
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require("./crawl");
+const { normalizeUrl, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeUrl strip Protocol", () => {
@@ -31,3 +31,19 @@ test("normalizeUrl strip http", () => {
     const expected = "www.google.com/path";
     expect(actual).toBe(expected);
 });
+
+
+test("getURLsFromHTML", () => {
+    const inputHtmlBody = `
+    <html>
+    <body>
+    <a href="https://www.google.com/">Google</a>
+    <a href="https://www.cricbuzz.com/">Cricbuzz</a>
+    </body>
+    </html>
+    `;
+    const inputBaseURL = "https://www.google.com";
+    const actual = getURLsFromHTML(inputHtmlBody, inputBaseURL);
+    const expected = ["https://www.google.com/", "https://www.cricbuzz.com/"];
+    expect(actual).toEqual(expected);
+})
